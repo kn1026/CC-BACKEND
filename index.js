0eabd8b63549c185bc516ff7850a7c665e0eb8ab
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2018-03-21 01:32:33 -0400

    test

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ var stripe = require("stripe")("sk_test_ml6mfjJq69CvQYaPlCiwdOAp")
 const PORT = process.env.PORT || 5000
 var app = express()
 express()
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded ({
-      extended : true
-  }))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded ({
+      extended : true
+  }));
 
 app.post('/customers', (req, res) => {
     var email = req.body.email
@@ -31,4 +32,4 @@ app.post('/customers', (req, res) => {
       req.send(customer)
     })
 
-})
+});
