23102d88196a8100fa8ccadf5df39f8b39ec9ac5
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2018-03-21 01:47:48 -0400

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
-var bodyParser = require('body-parser')
-var stripe = require("stripe")("sk_test_ml6mfjJq69CvQYaPlCiwdOAp")
+const bodyParser = require('body-parser');
+const stripe = require("stripe")("sk_test_ml6mfjJq69CvQYaPlCiwdOAp")
 const PORT = process.env.PORT || 5000
 var app = express()
 express()
