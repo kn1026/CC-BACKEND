6d7e778c37601138a6b429ce8aa902e79db55d56
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2018-03-21 01:57:11 -0400

    messaged

--- index.js
+++ index.js
@@ -1,41 +1,27 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
-const stripe = require("stripe")("sk_test_ml6mfjJq69CvQYaPlCiwdOAp")
-const PORT = process.env.PORT || 5000
-var app = express()
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const stripe = require("stripe")("sk_test_ml6mfjJq69CvQYaPlCiwdOAp");
+var app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded ({
       extended : true
   }));
 
+app.use(express.static(__dirname + 'public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+
+
 app.post('/customers', (req, res) => {
 
     console.log(req.body)
 
-    var email = req.body.email
-    var source = req.body.source
-
-    
-
-
-    stripe.customers.create({
-      email: email,
-      source: source,
-    }, function(err, customer) {
-      // asynchronously called
-      if(err != null) {
-
-        console.log(err)
-
-      }
-      res.send(customer)
-    })
 
 });
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
