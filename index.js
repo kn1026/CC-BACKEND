1390513f9b496c08701504b67f5fbf730cc76457
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2018-03-21 01:08:05 -0400

    test

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
+var bodyParser = require('body-parser')
+var stripe = require("stripe")("sk_test_ml6mfjJq69CvQYaPlCiwdOAp")
 const PORT = process.env.PORT || 5000
 
 express()
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded ({
+      extended : true
+  }))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
