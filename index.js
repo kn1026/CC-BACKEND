89127c2e645248ace17a75b900a215f8df185d8a
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2018-04-01 01:09:38 -0400

    changed

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const stripe = require("stripe")("sk_test_ml6mfjJq69CvQYaPlCiwdOAp");
 var app = express();
 const PORT = process.env.PORT || 5000;
 
-
+const accountSid = 'AC890d1e64b6af132a97b58edcd84acaee';
+const authToken = 'c9aa2f947339f8f124e19643a955a256';
+const client = require('twilio')(accountSid, authToken);
 
 
 app.use(bodyParser.json());
