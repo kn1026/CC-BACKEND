1b3155594ed751812c51879976abe2e2d6402bbd
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2018-04-10 03:59:17 -0400

    changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const stripe = require("stripe")("sk_live_MOC1tbrlvBZENX8WMEXiLhla");
+const stripe = require("stripe")("sk_test_ml6mfjJq69CvQYaPlCiwdOAp");
 var app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -227,7 +227,7 @@ app.post('/pre_authorization_apple_pay', (req, res) => {
 
     stripe.charges.create({
 
-      //customer: cus_id,
+      customer: cus_id,
       source: token,
       amount: amount,
       currency: "usd",
