4c6b3f59fd50cab6abda3da5d0fbd1ffcebfb7a6
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2018-08-04 19:36:16 +0700

    change test key

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const stripe = require("stripe")("sk_live_MOC1tbrlvBZENX8WMEXiLhla");
+const stripe = require("stripe")("sk_test_ml6mfjJq69CvQYaPlCiwdOAp");
 var app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -240,7 +240,6 @@ app.post('/pre_authorization', (req, res) => {
     var captured = req.body.captured
     var description = req.body.description
     var receipt_email = req.body.receipt_email
-
 
 
     stripe.charges.create({
