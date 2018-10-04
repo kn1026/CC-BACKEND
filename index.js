a0591103992ed3fe3559c75f2e90a404fcd00681
Khoi Nguyen <admin@craccteam.com>
2018-10-03 22:49:14 -0400

    checkr

--- index.js
+++ index.js
@@ -392,7 +392,7 @@ app.post('/checkRCreateCandidate', (req, res) => {
         headers: {
           'Content-Type': 'application/json',
           'X-User-Agent': 'Checkr.2.0.0.js',
-          'Authorization': req.body.YOUR_TEST_API_KEY
+          'Authorization': 'Basic ' + req.body.YOUR_TEST_API_KEY
         }
 
   };
