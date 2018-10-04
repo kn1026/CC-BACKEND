8add59a86ca9aee65d8b32a4057bd8249c8d6f3c
Khoi Nguyen <admin@craccteam.com>
2018-10-03 21:34:38 -0400

    checkr

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-import checkr from 'node-checkr';
+
 const path = require('path');
 const bodyParser = require('body-parser');
 const stripe = require("stripe")("sk_live_MOC1tbrlvBZENX8WMEXiLhla");
-const Checkr = new checkr("9fff2ef9b7167479110102a099c2d4175d36f775");
 var app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -14,6 +13,8 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const http = require('http');
 const https = require('https');
 const request = require('request');
+import checkr from 'node-checkr';
+const Checkr = new checkr("9fff2ef9b7167479110102a099c2d4175d36f775");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded ({
