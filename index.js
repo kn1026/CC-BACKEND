8106bd60f37d64b82bc15640ac0f8f01a3b91fa5
Khoi Nguyen <admin@craccteam.com>
2018-10-03 22:13:49 -0400

    checkr

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-
+import checkr from 'node-checkr';
 const path = require('path');
 const bodyParser = require('body-parser');
 const stripe = require("stripe")("sk_live_MOC1tbrlvBZENX8WMEXiLhla");
